b974b3ccd2c2e9099506e87cdee41defb078f30b
usuario <hamana@gmail.com>
2018-06-08 14:16:43 -0500

    Casi arreglado

--- app/src/main/java/com/example/chelohholmes/feats/MainActivity.java
+++ app/src/main/java/com/example/chelohholmes/feats/MainActivity.java
@@ -3,20 +3,28 @@ package com.example.chelohholmes.feats;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.chelohholmes.feats.R;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     public EditText numero;
     public TextView resultado;
+    public Button boton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,29 +32,36 @@ public class MainActivity extends AppCompatActivity {
 
         numero = (EditText) findViewById(R.id.Number);
         resultado = (TextView) findViewById(R.id.Resultado);
-    }
+        boton = (Button) findViewById(R.id.Button);
 
-    public void sacarFactorial (View v)
-    {
-        JsonObjectRequest peticion = new JsonObjectRequest
-                (
-                        Request.Method.GET,
-                        "http://192.168.84.33:8080/xampp/htdocs/Hamana.php?algo=", numero.getText().toString(),
-                        null,
-                        new Response.Listener<JSONObject>()
-                        {
-                            @Override
-                            public void onResponse (JSONObject response)
-                            {
-                                try{
-                                    int factorial =
-                                    //Resultado.setText("" + response.getInt("Factorial"));
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JsonObjectRequest peticion = new JsonObjectRequest
+                        (
+                                Request.Method.GET,
+                                "http://192.168.84.33:8080/xampp/htdocs/Hamana.php?algo="+ numero.getText().toString(),
+                                null,
+                                new Response.Listener<JSONObject>() {
+                                    @Override
+                                    public void onResponse(JSONObject response) {
+                                        try {
+                                            int factorial = response.getInt("Factorial");
+                                            resultado.setText("" + factorial);
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
                                 }
+                                , new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
                             }
-                        }
-                );
+                        });
+                RequestQueue x = Volley.newRequestQueue(MainActivity.this);
+                x.add(peticion);
+            }
+        });
     }
-
 }
-
-
